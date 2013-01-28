6f602de84865e44b99d2839e8520a4be49138c75
Devedse <davydavidse@gmail.com>
2013-01-28 13:35:48 +0100

    Fixed Mono Project

--- DeveMazeGeneratorConsole/Program.cs
+++ DeveMazeGeneratorConsole/Program.cs
@@ -11,39 +11,42 @@ using System.Threading;
 
 namespace DeveMazeGeneratorConsole
 {
-    class Program
-    {
-        public Program()
-        {
-            Directory.CreateDirectory("mazes");
-
-            Stopwatch w = new Stopwatch();
-            w.Start();
-
-            Algorithm alg = new AlgorithmBacktrack();
-            ParallelOptions options = new ParallelOptions();
-            //options.MaxDegreeOfParallelism = 8;
-            Parallel.For(0, 10000, options, new Action<int>((i) =>
-            {
-                if (i % 50 == 0)
-                {
-                    Console.WriteLine(i / 10 + "%");
-                }
-                int size = 256;
-                Maze maze = alg.Generate(size, size, i);
-                var path = PathfinderDepthFirst.GoFind(new MazePoint(1, 1), new MazePoint(size - 3, size - 3), maze.InnerMap);
-                //maze.SaveMazeAsBmpWithPath2("mazes\\" + i + ".bmp", path);
-            }));
-
-            w.Stop();
-            Console.WriteLine("Done: " + w.Elapsed.TotalSeconds);
+	class Program
+	{
+		public Program ()
+		{
+			int size = 2048*8;
+			GenerateMazeWithThisTypeAndShowTime(InnerMapType.BitArreintjeFast, size);
+			GenerateMazeWithThisTypeAndShowTime(InnerMapType.BitArreintjeFast, size);
 			Thread.Sleep(5000);
-        }
+			GenerateMazeWithThisTypeAndShowTime(InnerMapType.BitArreintjeFast, size);
 
+			Thread.Sleep (5000);
+		}
 
-        static void Main(string[] args)
-        {
-            Program p = new Program();
-        }
-    }
+		private void GenerateMazeWithThisTypeAndShowTime(InnerMapType type, int size)
+		{
+			Stopwatch w = new Stopwatch();
+			w.Start();
+			
+			AlgorithmBacktrack back = new AlgorithmBacktrack();
+			DebugMSG("Generating maze of type: " + type + " of size: " + size);
+			Maze maze = back.Generate(size, size, type, null);
+			//var path = PathFinderDepthFirst.GoFind(new MazePoint(1, 1), new MazePoint(size - 3, size - 3), maze.InnerMap);
+			//maze.SaveMazeAsBmpWithPath4bpp("maze.bmp", path);
+			DebugMSG("Ok done, time: " + w.Elapsed.TotalSeconds);
+			DebugMSG("");
+			
+		}
+
+		static void Main (string[] args)
+		{
+			Program p = new Program ();
+		}
+
+		public void DebugMSG (String str)
+		{
+			Console.WriteLine (str);
+		}
+	}
 }
