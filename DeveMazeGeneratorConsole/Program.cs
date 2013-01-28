e96a6ea1ad3f6fa1be033a85b7add19222eb7af3
Devedse <davydavidse@gmail.com>
2013-01-28 13:56:19 +0100

    Ctrl K + D

--- DeveMazeGeneratorConsole/Program.cs
+++ DeveMazeGeneratorConsole/Program.cs
@@ -15,27 +15,27 @@ namespace DeveMazeGeneratorConsole
 	{
 		public Program ()
 		{
-			int size = 2048*8;
-			GenerateMazeWithThisTypeAndShowTime(InnerMapType.BitArreintjeFast, size);
-			GenerateMazeWithThisTypeAndShowTime(InnerMapType.BitArreintjeFast, size);
-			Thread.Sleep(5000);
-			GenerateMazeWithThisTypeAndShowTime(InnerMapType.BitArreintjeFast, size);
+			int size = 2048 * 8;
+			GenerateMazeWithThisTypeAndShowTime (InnerMapType.BitArreintjeFast, size);
+			GenerateMazeWithThisTypeAndShowTime (InnerMapType.BitArreintjeFast, size);
+			Thread.Sleep (5000);
+			GenerateMazeWithThisTypeAndShowTime (InnerMapType.BitArreintjeFast, size);
 
 			Thread.Sleep (5000);
 		}
 
-		private void GenerateMazeWithThisTypeAndShowTime(InnerMapType type, int size)
+		private void GenerateMazeWithThisTypeAndShowTime (InnerMapType type, int size)
 		{
-			Stopwatch w = new Stopwatch();
-			w.Start();
+			Stopwatch w = new Stopwatch ();
+			w.Start ();
 			
-			AlgorithmBacktrack back = new AlgorithmBacktrack();
-			DebugMSG("Generating maze of type: " + type + " of size: " + size);
-			Maze maze = back.Generate(size, size, type, null);
+			AlgorithmBacktrack back = new AlgorithmBacktrack ();
+			DebugMSG ("Generating maze of type: " + type + " of size: " + size);
+			Maze maze = back.Generate (size, size, type, null);
 			//var path = PathFinderDepthFirst.GoFind(new MazePoint(1, 1), new MazePoint(size - 3, size - 3), maze.InnerMap);
 			//maze.SaveMazeAsBmpWithPath4bpp("maze.bmp", path);
-			DebugMSG("Ok done, time: " + w.Elapsed.TotalSeconds);
-			DebugMSG("");
+			DebugMSG ("Ok done, time: " + w.Elapsed.TotalSeconds);
+			DebugMSG ("");
 			
 		}
 
